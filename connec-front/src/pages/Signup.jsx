ff4e5a6c6bf4ba595a9b2544fa80e4c5ec3eb7d7
mohakksingh <singhmohak999@gmail.com>
2025-01-28 22:23:22 +0530

    updated bg

--- connec-front/src/pages/Signup.jsx
+++ connec-front/src/pages/Signup.jsx
@@ -54,7 +54,7 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center  px-4 py-12 sm:px-6 lg:px-8">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl font-bold text-center">
